22d7c4919b49209747d8752d5d2126ca53811723
GertkingABP <kozyakinandrey34@mail.ru>
2024-11-11 14:52:33 +0300

    Modified YAML data reader

--- test/test_YamlDataReader.py
+++ test/test_YamlDataReader.py
@@ -3,33 +3,29 @@ from src.YamlDataReader import YamlDataReader
 import yaml
 
 
-@pytest.fixture
-def yaml_test_data(tmp_path):
-    """Фикстура создания врем-го YAML-файла с тестовыми данными"""
-    test_data = {
-        'students': [
-            {'name': 'John Doe', 'scores': [100, 96, 97]},
-            {'name': 'Jane Brown', 'scores': [99, 89, 90]},
-            {'name': 'Mark Spencer', 'scores': [89, 88, 87]}
-        ]
-    }
+class TestYamlDataReader:
+    @pytest.fixture
+    def yaml_data_reader(self, tmp_path):
+        """Создание временного YAML файла и объекта для чтения данных"""
+        test_data = {
+            'students': [
+                {'name': 'John Doe', 'scores': [100, 96, 97]},
+                {'name': 'Jane Brown', 'scores': [99, 89, 90]},
+                {'name': 'Mark Spencer', 'scores': [89, 88, 87]}
+            ]
+        }
 
-    yaml_file = tmp_path / "testfile.yaml"
-    with yaml_file.open('w') as f:
-        yaml.dump(test_data, f)
+        yaml_file = tmp_path / "testfile.yaml"
+        with yaml_file.open('w') as f:
+            yaml.dump(test_data, f)
 
-    return yaml_file
+        reader = YamlDataReader(str(yaml_file))
+        reader.read_data()
+        return reader
 
-
-@pytest.fixture
-def yaml_data_reader(yaml_test_data):
-    """Фикстура создания объекта и чтения данных из врем-го файла"""
-    reader = YamlDataReader(str(yaml_test_data))
-    reader.read_data()
-    return reader
-
-
-def test_read_data(yaml_data_reader):
-    """Тест успешной загрузки данных"""
-    assert yaml_data_reader.data is not None, "Data loaded from YAML file."
-    assert len(yaml_data_reader.data['students']) == 3, "Should be all stud-s."
+    def test_read_data(self, yaml_data_reader):
+        """Тест успешной загрузки данных"""
+        assert yaml_data_reader.data is not None, \
+            "Sh-ld l-ded from YAML f-e."
+        assert len(yaml_data_reader.data['students']) == 3, \
+            "S-ld be all st-s."
